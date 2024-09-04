4aa553347ea48e230608935859bfddc4e0716a7b
carvaled <69370785+carvaled@users.noreply.github.com>
2024-09-04 19:43:22 -0300

    Configuração do Swagger com autenticação Bearer Token

--- SpotifyLike.Api/Program.cs
+++ SpotifyLike.Api/Program.cs
@@ -1,18 +1,61 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Spotify.Application.Conta;
 using Spotify.Application.Conta.Profile;
 using Spotify.Application.Streaming;
 using SpotifyLike.Repository;
 using SpotifyLike.Repository.Repository;
 
+var appName = "Web Api Spotify Like";
+var appVersion = "v1";
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSwaggerGen(c =>
+{
+
+
+    c.SwaggerDoc(appVersion,
+    new OpenApiInfo
+    {
+        Title = appName,
+        Version = appVersion,
+        Description = "API Serviços de Streaming Spotify Like.",
+        Contact = new OpenApiContact
+        {
+            Name = "Eduardo Carvalho ",
+            Url = new Uri("https://github.com/carvaled/SpotifyLike_2.0_2")
+        }
+    });
+
+    c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme()
+    {
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        Description = "Adicione o token JWT para fazer as requisições na APIs",
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 builder.Services.AddDbContext<SpotifyLikeContext>(c =>
 {
@@ -37,11 +80,11 @@ builder.Services.AddScoped<BandaService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
+// if (app.Environment.IsDevelopment()) { 
     app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{appName} {appVersion}"); });
+//}
 
 app.UseHttpsRedirection();
 
